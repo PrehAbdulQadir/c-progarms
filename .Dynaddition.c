8274b337119972bdc8e55f54fe69e59e32b01d77
PrehAbdulQadir <136431070+PrehAbdulQadir@users.noreply.github.com>
2023-07-05 10:29:14 +0500

    Add files via upload

---
+++ .Dynaddition.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void main()
+{
+	int v1,v2;
+
+	printf("Enter 1st integer number ");
+	scanf("%d",&v1);
+	printf("Enter 2and integer number ");
+	scanf("%d",&v2);
+	
+	//output
+	printf("\naddition of %d and %d = %d     ",v1,v2,v1+v2);
+	printf("\nsubtarctation of %d and %d = %d ",v1,v2,v1-v2);
+	printf("\nmultiplyies of %d and %d = %d   ",v1,v2,v1*v2);
+    printf("\ndivid of  %d and %d = %d        ",v1,v2,v1/v2);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
