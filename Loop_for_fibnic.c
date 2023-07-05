374aaf5179207bd09c6a125aca9bf56fc256a40c
PrehAbdulQadir <136431070+PrehAbdulQadir@users.noreply.github.com>
2023-07-05 10:40:17 +0500

    Add files via upload

---
+++ Loop_for_fibnic.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int i,num1,num2,perN,count;
+	printf("    Enter the first number ");
+	scanf ("%d",&i);
+	printf("    Enter the count number ");
+	scanf ("%d",&count);
+	for(i;i<=count;i++)
+	{
+	   printf("%d",num1);
+	   perN=num1+num2;
+	   num1=num2;
+	   num2=perN;    	
+      		
+		
+		
+		
+    }
+	
+	
+	
+	
+	
+}
