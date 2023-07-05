258dcb1cc828555ea0405c85280ae4967b3c17c2
PrehAbdulQadir <136431070+PrehAbdulQadir@users.noreply.github.com>
2023-07-05 10:27:04 +0500

    Add files via upload

---
+++ conditions.c
@@ -0,0 +1,69 @@
+void main()
+{
+ int a,b;	
+		
+	printf("   Enter a Value ");
+ 	scanf("%d",&a);
+    printf("   Enter b Value ");
+ 	scanf("%d",&b);
+   if(a>b)
+   {   
+        printf("  a is bigger than b ");
+   }
+  else if(a<b) 
+  
+  {
+        printf(" b is bigger than a");
+  }
+  else if(a==b)
+
+   {  
+        printf(" a is equale to b");	
+   } 
+  else
+   {
+	    printf("no one contion is ture  ");
+   }
+	
+ } 
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
