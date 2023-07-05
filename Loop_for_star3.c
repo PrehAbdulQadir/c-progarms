6a922d78d43a8814f570b2c0f75dd6f343af4db6
PrehAbdulQadir <136431070+PrehAbdulQadir@users.noreply.github.com>
2023-07-05 10:41:06 +0500

    Add files via upload

---
+++ Loop_for_star3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		printf(" \n ");
+		for(j=0;j<=i;j++)
+		{
+			printf("\t*");
+			
+		}
+	
+		
+	}
+	for(i=1;i<=5;i++)
+	{
+		printf("\n");
+		for(j=i;j<=5;j++)
+		{
+			printf("\t*");
+		}
+		    
+			
+    }
+			
+			
+			
+}
+
+
+
+
+	
