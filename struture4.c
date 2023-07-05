aa41439526f89bbbb65b9ccd3a1029f1e1d28e2b
PrehAbdulQadir <136431070+PrehAbdulQadir@users.noreply.github.com>
2023-07-05 10:41:53 +0500

    Add files via upload

---
+++ struture4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+struct size
+{
+	int a;   // 2 Biytes
+	char b;  // 1 Biyte
+	float c; // 4 Bytes
+};
+void main()
+{
+	struct size s;
+	printf("Manimum size of struct of (sizeof( size.s)");
+}
+
+
