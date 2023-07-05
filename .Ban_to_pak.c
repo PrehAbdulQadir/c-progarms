c404958aee956fcccd310576e0e177bc25973d88
PrehAbdulQadir <136431070+PrehAbdulQadir@users.noreply.github.com>
2023-07-05 10:28:46 +0500

    Add files via upload

---
+++ .Ban_to_pak.c
@@ -0,0 +1,13 @@
+void main(void)
+{
+	float bangal_Rs;
+	float pak_Rs;
+	printf("\n\tEnter Bangal currency       ");
+	scanf ("%f",&bangal_Rs);
+	
+	pak_Rs=bangal_Rs/2 ;
+	printf("\tpak Rs %f will be %f ",pak_Rs,bangal_Rs);
+	
+	
+	
+}
